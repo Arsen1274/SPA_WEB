0871c712f858369a28741cce76c2bd414fedbfca
Arsen Sirkovych <arsenchik2001a@gmail.com>
2021-03-27 22:16:29 +0200

    Header complete
    Fully completed header with gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp');
+var pug = require('gulp-pug');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var fileinclude = require('gulp-file-include');
 
 function copy(done){
-  gulp.src('./scss/**/*.scss')
+  gulp.src('./src/scss/*.scss')
     .pipe(sass({
       errorLogToConsole: true
     }))
     .on('eroor', console.error.bind(console))
     //.pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./css/'))
+    .pipe(gulp.dest('./build/css/'))
     .pipe(browserSync.stream());
 
     console.log('Update scss->css');
@@ -19,14 +21,14 @@ function copy(done){
 
 
 function watchSass(){
-  gulp.watch('./scss/**/*',copy)
+  gulp.watch('./src/scss/*',copy)
 }
 
 
 function sync(done){
   browserSync.init({
     server:{
-      baseDir:'./'
+      baseDir:'./build'
     },
     port:3000
   });
@@ -39,11 +41,41 @@ function browserReload(done){
 }
 
 function watchFiles(){
-  gulp.watch('./scss/**/*',copy);
-  gulp.watch('./**/*.html',browserReload);
+  gulp.watch('./src/scss/**/*',copy);
+  gulp.watch('./**/*.html',buildHTML);
+//  gulp.watch('./**/*.html',browserReload);
   gulp.watch('./**/*.js',browserReload);
 }
-gulp.task('default',gulp.parallel(sync,watchFiles));
 
-//exit watchSacc  ctrl+c
+function buildHTML(done) {
+  return gulp.src('./src/pug/**/*.pug')
+  .pipe(pug({
+    pretty:true
+  }))
+  .pipe(gulp.dest('./build'))
+  browserSync.reload();
+done();
+
+}
+
+function includeMy(done) {
+  gulp.src(['./build/index.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./build'));
+  done();
+}
+
+function html(){
+  return gulp.src('./src/index.html')//path.src.html)
+    .pipe(fileinclude())
+    .pipe(gulp.dest('./build'))//path.build.html))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('default',gulp.parallel(sync,watchFiles,buildHTML,html));
+
+//exit watchSacc
 //gulp.task(copy);
