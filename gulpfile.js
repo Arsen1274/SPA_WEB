cf19f88ec4b6053da5aad2221074fd37e71c4e5b
Arsen Sirkovych <arsenchik2001a@gmail.com>
2021-03-30 22:40:16 +0300

    page2.html Component tableRow.html
    Pages: page2.html
    Component: tableRow.htmlV1.0
    Index.html can find page.html
     page.html can find component.html

--- gulpfile.js
+++ gulpfile.js
@@ -42,20 +42,30 @@ function browserReload(done){
 
 function watchFiles(){
   gulp.watch('./src/scss/**/*',copy);
-  gulp.watch('./**/*.html',buildHTML);
-//  gulp.watch('./**/*.html',browserReload);
+gulp.watch('./src/scss/**/*',browserReload);
+  //gulp.watch('.//*.html',buildHTML);
+  gulp.watch('./**/*.html',browserReload);
   gulp.watch('./**/*.js',browserReload);
 }
 
-function buildHTML(done) {
-  return gulp.src('./src/pug/**/*.pug')
-  .pipe(pug({
-    pretty:true
-  }))
-  .pipe(gulp.dest('./build'))
-  browserSync.reload();
-done();
 
+
+function buildComponents(){
+    //.pipe(fileinclude())
+   return gulp.src('./src/html/components/*.html')
+    .pipe(gulp.dest('./build/html/'))//path.build.html))
+    .pipe(browserSync.stream());
+    //.pipe(console.log("buildComponents DONE"));
+  //done();
+}
+function buildPages(){
+  //return gulp.src('F:/КПІ/3 курс/2 семестр/Веб/лаб 1/Project/src/html/pages/page2.html')
+   return gulp.src('./src/html/pages/page2.html')  //path.src.html)
+    .pipe(fileinclude())
+    .pipe(gulp.dest('./build/html/'))//path.build.html))
+    .pipe(browserSync.stream());
+    //.pipe(console.log("buildPages DONE"));
+  //done();
 }
 
 function includeMy(done) {
@@ -69,13 +79,31 @@ function includeMy(done) {
 }
 
 function html(){
+  //gulp.buildComp;
+  //gulp.start('buildComp');
+  //buildComp;
+  //gulp.task('buildComp',buildComponents);
+  console.log("func html: buildComponents DONE");
+  //buildPages();
+  console.log("func html: buildPages DONE");
   return gulp.src('./src/index.html')//path.src.html)
     .pipe(fileinclude())
     .pipe(gulp.dest('./build'))//path.build.html))
     .pipe(browserSync.stream());
 }
 
-gulp.task('default',gulp.parallel(sync,watchFiles,buildHTML,html));
+gulp.task('default2', gulp.parallel(sync,watchFiles,html));
+//gulp.task('default', gulp.parallel(sync,watchFiles,html));
+gulp.task('buildComp',buildComponents);
+gulp.task('buildPg',buildPages);
+
+//gulp.task('default',['buildComp','default1']);
+
+//gulp.task('default',['dist','default2']);
+//gulp.task('default2', ['buildComp', 'default1']);
+
+gulp.task('default',gulp.series("buildComp","buildPg",gulp.parallel(sync,watchFiles,html)))
+
 
 //exit watchSacc
 //gulp.task(copy);
